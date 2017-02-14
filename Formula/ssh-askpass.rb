fb1bb985db89585c1ec61857f0f5879074e094f5
Tom Maher <tmaher@github.com>
2017-02-13 16:17:21 -0800

    more shenanigans

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -12,6 +12,11 @@ class SshAskpass < Formula
     bin.install 'ssh-askpass'
   end
 
+  def ssh_add_cmd
+    ":" unless Formula['go-ssh-add'] && Formula['go-ssh-add'].opt_bin
+    File.join(Formula['go-ssh-add'].opt_bin, "go-ssh-add")
+  end
+
   def plist; <<-EOS.undent
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -24,13 +29,9 @@ class SshAskpass < Formula
       <string>/bin/sh</string>
       <string>-c</string>
       <string>
-        /bin/launchctl setenv SSH_ASKPASS \"#{opt_bin}/ssh-askpass\"
-        if [ -z $DISPLAY ]; then
-          /bin/launchctl setenv DISPLAY \"#{DISPLAY_TEXT}\"
-        fi
-        if [ -x \"#{opt_bin}/go-ssh-add\" ]; then
-          \"#{opt_bin}/go-ssh-add\" add-all
-        fi
+        /bin/launchctl setenv SSH_ASKPASS '#{opt_bin}/ssh-askpass'
+        /bin/launchctl setenv DISPLAY \"${DISPLAY:-#{DISPLAY_TEXT}}\"
+        '#{ssh_add_cmd}' add-all 2>/dev/null
       </string>
     </array>
     <key>RunAtLoad</key>
