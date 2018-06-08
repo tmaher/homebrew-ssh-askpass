4ff61b0194cb4821c4892cb9d232ea0406a42cc2
Tom Maher <tmaher@users.noreply.github.com>
2018-06-08 16:13:38 +0100

    use ~

--- Formula/ssh-askpass.rb
+++ Formula/ssh-askpass.rb
@@ -18,7 +18,7 @@ class SshAskpass < Formula
       true"
   end
 
-  def plist; <<-EOS.undent
+  def plist; <<~EOS.undent
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
